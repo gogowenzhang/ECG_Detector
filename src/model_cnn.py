24af463e61b5f5713440c27a5062beccd8aa8d9e
Wen Zhang <wen@Brians-MBP.attlocal.net>
2018-01-09 23:25:19 -0800

    add call_back

--- src/model_cnn.py
+++ src/model_cnn.py
@@ -4,9 +4,12 @@ import pickle
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda, Reshape
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Activation, Bidirectional, LSTM
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from keras import backend as K
 from sklearn.metrics import confusion_matrix
 from myfunctions import add_conv_blocks
+
+MODEL_NAME = 'cnn3'
 
 print('Loading data...')
 with open('x_s_whole.pkl', 'rb') as f:
@@ -14,6 +17,16 @@ with open('x_s_whole.pkl', 'rb') as f:
     
 with open('y_whole.pkl', 'rb') as f:
     y = pickle.load(f)
+
+# Split train and test
+obs_n = x_s.shape[0]
+train_n = int(x_s.shape[0] * 0.8)
+ind_selected = np.random.choice(obs_n, train_n,replace=False)
+ind_not_selected = [i for i in x_s if i not in ind_selected]
+x_train = np.array([x_s[i] for i in ind_selected])
+x_test = np.array([x_s[i] for i in ind_not_selected])
+y_train = y[ind_selected]
+y_test = y[ind_not_selected]
 
 # Hyperparameter
 batch_size = 20
@@ -47,14 +60,23 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               metrics=['accuracy'])
 
 print('Train...')
-model.fit(x_s, y, batch_size=batch_size,
-          epochs=epochs, verbose=1)
+saved_model_file = './trained_model_{}.h5'.format(MODEL_NAME)
+# We use early stopping to avoid spending time on overfitting our model
+early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+# save model at checkpoints when loss function improved
+checkpoint = ModelCheckpoint(saved_model_file, monitor='val_loss', save_best_only=True, verbose=1)
+# and keep logs for visualisation with TensorBoard
+tensorboard = TensorBoard('./tensorboard_logs', histogram_freq=1)
+
+model.fit(x_train, y_train, batch_size=batch_size,
+          epochs=epochs, verbose=1, 
+          callbacks=[tensorboard, checkpoint, early_stopping])
 
 
 print('Evaluation...')
-y_predict = model.predict(x_s)
+y_predict = model.predict(x_test)
 
-acc = np.mean(y_predict.argmax(axis=1) == y.argmax(axis=1))
+acc = np.mean(y_predict.argmax(axis=1) == y_test.argmax(axis=1))
 print 'accuracy', acc
 
-print confusion_matrix(y.argmax(axis=1), y_predict.argmax(axis=1))
+print confusion_matrix(y_test.argmax(axis=1), y_predict.argmax(axis=1))
